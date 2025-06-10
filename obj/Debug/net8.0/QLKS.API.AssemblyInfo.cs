3a69717871ef882675aad1f1d2a7a51d3e994abc
tuana <tuana@LETUANANH>
2025-06-10 17:31:25 +0700

    Update master6

--- obj/Debug/net8.0/QLKS.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/QLKS.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QLKS.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f41c38cd6f73cfeee9c160b0bb350cf56367add4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4de874094ccffa4855764ba850a682aa13a54f7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("QLKS.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QLKS.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
